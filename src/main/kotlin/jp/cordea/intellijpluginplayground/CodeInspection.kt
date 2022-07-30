ed9c7e05fa180563f3be0c842964f7fca7168cc6
Yoshihiro Tanaka <contact@cordea.jp>
2022-07-30 16:19:12 +1000

    implement a quick-fix

--- src/main/kotlin/jp/cordea/intellijpluginplayground/CodeInspection.kt
+++ src/main/kotlin/jp/cordea/intellijpluginplayground/CodeInspection.kt
@@ -1,14 +1,16 @@
 package jp.cordea.intellijpluginplayground
 
 import com.intellij.codeInspection.LocalInspectionTool
+import com.intellij.codeInspection.LocalQuickFix
+import com.intellij.codeInspection.ProblemDescriptor
 import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiElementVisitor
 import org.jetbrains.kotlin.idea.debugger.sequence.psi.callName
-import org.jetbrains.kotlin.idea.refactoring.changeSignature.usages.explicateReceiverOf
-import org.jetbrains.kotlin.idea.references.mainReference
 import org.jetbrains.kotlin.psi.KtCallExpression
 import org.jetbrains.kotlin.psi.KtDotQualifiedExpression
+import org.jetbrains.kotlin.psi.KtPsiFactory
 
 class CodeInspection : LocalInspectionTool() {
     override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor = Visitor(holder)
@@ -20,9 +22,21 @@ private class Visitor(private val holder: ProblemsHolder) : PsiElementVisitor() 
         if (element !is KtDotQualifiedExpression || element.receiverExpression.text != "Log") {
             return
         }
+        holder.registerProblem(element, "Avoid using Log, consider using Timber instead.", QuickFix())
+    }
+}
+
+private class QuickFix : LocalQuickFix {
+    override fun getFamilyName(): String = "Replace with Timber"
+
+    override fun applyFix(project: Project, descriptor: ProblemDescriptor) {
+        val element = descriptor.psiElement as KtDotQualifiedExpression
         val expression = (element.selectorExpression as? KtCallExpression) ?: return
         val level = expression.callName()
-        val args = expression.valueArguments.map { it.name }
-        holder.registerProblem(element, "Avoid using Log, consider using Timber instead.")
+        val args = expression.valueArguments.map { it.text }
+        element.replace(
+                KtPsiFactory(project)
+                        .createExpression("Timber.${level}(${args[0]}, ${args[1]})")
+        )
     }
 }
