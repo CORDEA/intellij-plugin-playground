8ef234031215d958f4b58ae8d33570708dc76686
Yoshihiro Tanaka <contact@cordea.jp>
2022-07-30 15:45:56 +1000

    add log inspection

--- src/main/kotlin/jp/cordea/intellijpluginplayground/CodeInspection.kt
+++ src/main/kotlin/jp/cordea/intellijpluginplayground/CodeInspection.kt
@@ -4,13 +4,25 @@ import com.intellij.codeInspection.LocalInspectionTool
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiElementVisitor
+import org.jetbrains.kotlin.idea.debugger.sequence.psi.callName
+import org.jetbrains.kotlin.idea.refactoring.changeSignature.usages.explicateReceiverOf
+import org.jetbrains.kotlin.idea.references.mainReference
+import org.jetbrains.kotlin.psi.KtCallExpression
+import org.jetbrains.kotlin.psi.KtDotQualifiedExpression
 
 class CodeInspection : LocalInspectionTool() {
-    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor = Visitor()
+    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor = Visitor(holder)
 }
 
-private class Visitor : PsiElementVisitor() {
+private class Visitor(private val holder: ProblemsHolder) : PsiElementVisitor() {
     override fun visitElement(element: PsiElement) {
         super.visitElement(element)
+        if (element !is KtDotQualifiedExpression || element.receiverExpression.text != "Log") {
+            return
+        }
+        val expression = (element.selectorExpression as? KtCallExpression) ?: return
+        val level = expression.callName()
+        val args = expression.valueArguments.map { it.name }
+        holder.registerProblem(element, "Avoid using Log, consider using Timber instead.")
     }
 }
